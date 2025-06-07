2a5f22402b026f7b7b85d94d55870097b0f46f9b
Strops91 <pedrovini91@gmail.com>
2025-06-07 19:09:52 -0300

    atualizacao App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Detail from './components/Dados';
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/post" element={<Lista />} />
       <Route path="/dados/:id" element={<Detail />} />
       <Route path="*" element={<Navigate to="/" />} />
