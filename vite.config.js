6f9fcc3648506fde8ae6906831cbbe46712f4caa
Strops91 <pedrovini91@gmail.com>
2025-06-07 21:10:51 -0300

    Atualização arquivos .json

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,37 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: /^@mui\/material\/(.*)/,
+        replacement: '@mui/material/$1'
+      },
+      {
+        find: /^@mui\/icons-material\/(.*)/,
+        replacement: '@mui/icons-material/$1'
+      }
+    ]
+  },
   optimizeDeps: {
     include: [
       '@mui/material',
       '@emotion/react',
-      '@emotion/styled'
-    ]
+      '@emotion/styled',
+      '@mui/material/styles',
+      '@mui/material/CircularProgress',
+      // Adicione outros componentes MUI que você está usando
+    ],
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
   },
   build: {
     commonjsOptions: {
-      transformMixedEsModules: true
+      transformMixedEsModules: true,
+      include: [/node_modules/]
     }
   }
 });
